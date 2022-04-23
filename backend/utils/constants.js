11c1f73e6aad7bfbfbcab360ea8af0f56c7c597a
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-04-23 15:40:28 +0100

    Added more crontab times

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -3,9 +3,11 @@ import { PopulationModel } from "../models/populationModel.js";
 
 // Crontab constants
 export const CRON_TIME = '* * * * *';
+export const CRON_TIME_MONTH = '0 0 1 * *';
+export const CRON_TIME_YEAR = '0 0 1 1 *';
 
 // Database constants
-export const COVID_TABLE_NAME = 'defunctions_covid'; 
+export const COVID_TABLE_NAME = 'defunctions_covid';
 export const POPULATION_TABLE_NAME = 'population';
 
 export const database_models = {
