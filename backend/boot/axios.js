b548d182ce34773531a05bf001443b1fbeadbd35
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-04-23 14:32:53 +0100

    Boot components added

---
+++ backend/boot/axios.js
@@ -0,0 +1,54 @@
+//configuracion inicial de axios
+import axios from 'axios';
+import https from 'https';
+
+const api = axios.create({
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  })
+});
+
+console.log("Axios cargado correctamente.");
+
+async function getURL(url) {
+	let dataset;
+	
+	await api.get(url)
+		.then(function (response) {
+			if (response.status === 200) {
+				const distribution = response.data.result.items[0].distribution;
+
+				for (const element of distribution) {
+					if (element.format.value === 'application/json') {
+						dataset = element.accessURL;
+						return;
+					}
+				}
+			}
+			return dataset;
+		}).catch(function (error) {
+			console.log(error);
+		});
+
+	return dataset;
+}
+
+async function getJSON(url) {
+	let data = {};
+
+	await api.get(url)
+		.then(function (response) {
+			if (response.status === 200) {
+				data = response.data;
+				return data;
+			}
+		})
+		.catch(function (error) {
+			console.log(error);
+			return error;
+		}
+	);
+	return data;
+}
+
+export { api, getURL, getJSON };
---
+++ backend/boot/database.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+mongoose.connect( "mongodb://localhost:27017/Demografia", { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+})
+.then( db => console.log('DB is connected'))
+.catch( err => console.error(err)); 
+
+export {mongoose};
