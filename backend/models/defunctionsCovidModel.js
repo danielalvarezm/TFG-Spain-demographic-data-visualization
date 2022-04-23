b0d082fdb2aa0c64e7031641d9dff5232b592dd5
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-04-23 14:54:31 +0100

    Model syntax changed

--- backend/models/defunctionsCovidModel.js
+++ backend/models/defunctionsCovidModel.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 const CovidSchema = new mongoose.Schema({
-    id: { type: Number, required: true },
-    age: { type: String, required: true },
-    male_values: { type: Number, required: true }, 
-    female_values: { type: Number, required: true },
-    total_values: { type: Number, required: true }   
+  id: { type: Number, required: true },
+  age: { type: String, required: true },
+  male_values: { type: Number, required: true }, 
+  female_values: { type: Number, required: true },
+  total_values: { type: Number, required: true }   
 });
 
 const CovidModel = mongoose.model('Defunctions_covid', CovidSchema, 'defunctions_covid')
--- backend/models/natalityModel.js
+++ backend/models/natalityModel.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
 const NatalitySchema = new mongoose.Schema({
-    id: { type: Number, required: true },
-    ccaa: { type: String, required: true },
-    values: [{
-        interval: { type: Number, required: true },
-        value: { type: Number, required: true }
-    }],
+  id: { type: Number, required: true },
+  ccaa: { type: String, required: true },
+  values: [{
+    interval: { type: Number, required: true },
+    value: { type: Number, required: true }
+  }],
 });
 
 const NatalityModel = mongoose.model('Natality', NatalitySchema, 'natality');
