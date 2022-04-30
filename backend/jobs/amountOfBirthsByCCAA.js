bfda0cca406f72de33b0f447c06789eabd4b1e7b
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-05-01 00:31:00 +0100

    feat: merge datasets to obtain amount of births by ccaa

--- backend/jobs/amountOfBirthsByCCAA.js
+++ backend/jobs/amountOfBirthsByCCAA.js
@@ -1,6 +1,55 @@
 import { getNatalityData } from "../services/natalityByCCAAService";
 import { getPopulationData } from "../services/populationByCCAAService";
 
-// Obtain the data from the database
-const natalityData = getNatalityData();
-const populationData = getPopulationData();
+const finalDataset = getAmountOfBirthByCCAA();
+
+
+
+
+// Año 2020
+// Andalucía
+// Tasa natalidad: 7.72%
+// * Total: 8483769
+// * Mujer: 4298591
+// * Hombre: 4185178
+// Calculo (natality * population) / 1000
+// * Total -> (8483769 * 7.72) / 1000 = 65494,69668
+// * Hombre -> (4298591 * 7.72) / 1000 = 33185,12252
+// * Mujer -> (4185178 * 7.72) / 1000 = 32309,57416
+
+// Between 2002 and 2020
+// amountOfBirthsByCCAA = (natality * population) / 1000
+function getAmountOfBirthByCCAA() {
+  const natalityData = getNatalityData();
+  const populationData = getPopulationData();
+
+  let finalDataset = [];
+  for (let i = 0; i < natalityData.length; i++) {
+    const ccaa = natalityData[i].ccaa;
+    const natalities = natalityData[i].values;
+    let populations = populationData[i].total_values;
+    populations.shift();
+
+    let values = [];
+    for (let j = 0; j < natalities.length; j++) {
+      const year = natalities[j].interval;
+      const natality = natalities[j].value;
+      const population = populations[j].value;
+
+      values.push({
+        interval: year,
+        value: (natality * population) / 1000
+      });
+    }
+
+    finalDataset.push({
+      id: i,
+      ccaa: ccaa,
+      values: values
+    });
+  }
+
+  return finalDataset;
+}
+
+
