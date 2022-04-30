ad4c5f3afe293d9eb6bd8fecde12306ee630307d
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-05-01 00:33:39 +0100

    feat: insert amount of births by ccaa to database

---
+++ backend/services/amountOfBirthsByCCAAService.js
@@ -0,0 +1,19 @@
+import { AmountOfBirthsModel } from "../models/amountOfBirthsByCCAAModel";
+
+async function amountOfBirthsDataToDB (final_dataset) {
+  await AmountOfBirthsModel.deleteMany({});
+
+  for (let i = 0; i < final_dataset.length; i++) {
+    const data = final_dataset[i];
+    const newAmountOfBirths = new AmountOfBirthsModel({
+      id: data.id,
+      ccaa: data.ccaa,
+      values: data.values
+
+    });
+
+    await newAmountOfBirths.save();
+  }
+}
+
+export { amountOfBirthsDataToDB };
