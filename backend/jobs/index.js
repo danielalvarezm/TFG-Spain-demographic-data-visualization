2c676e480343f4a5e962425f1113fb038ab54c6d
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-05-01 17:05:04 +0100

    fix: await import for amount of births by ccaa

--- backend/jobs/index.js
+++ backend/jobs/index.js
@@ -1,5 +1,14 @@
 import './defunctionsCovid.js';
-import './populationByCCAA.js';
 import './fecundityByCCAA.js';
+import './populationByCCAA.js';
 import './natalityByCCAA.js';
-import './amountOfBirthsByCCAA.js';
+// wait one second
+setTimeout(function () {
+  import ('./amountOfBirthsByCCAA.js');
+},100)
+
+
+// import ('./populationByCCAA.js')
+//   .then(import ('./natalityByCCAA.js'))
+//   .then(import ('./amountOfBirthsByCCAA.js'))
+
